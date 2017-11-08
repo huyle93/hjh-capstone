9b9e902a102db7c31d29fb7eb67d63b6ca4e234c
Huy Le <huyle.finance@gmail.com>
2017-11-07 23:48:08 -0500

    working scraper for single page, converting to text for path is in progress

--- src/data_retriever/main_scraper.py
+++ src/data_retriever/main_scraper.py
@@ -7,6 +7,9 @@ Created on Tue Oct 31 13:37:51 2017
 """
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve
+import sys
+sys.path.insert(0, '../features')
+import pol_UNH.py
 
 
 from_year = input("from year: ")
@@ -16,12 +19,23 @@ vermont_site_url = "https://www.vermontjudiciary.org/opinions-decisions?facet_fr
 
 html = urlopen(vermont_site_url)
 bsObj = BeautifulSoup(html, "html.parser")
+
 pdf_url = bsObj.findAll("div", {"class": "views-field views-field-field-document"})
-count = 0
+url_list = []
 print("="*10+"STATE OF VERMONT"+"="*10)
-print(vermont_site_url)
+print("Data is pulling from this URL: " + vermont_site_url)
+print("="*10+"DONE"+"="*10)
+print("loading...")
 for link in pdf_url:
-    count += 1
-    print("="*10, count)
-    print(link.find('a').attrs['href'], "\n")
+#    print("="*10, count)
+    url_list.append(link.find('a').attrs['href'])
+#    print(link.find('a').attrs['href'], "\n")
 #print(pdf_url.get('href'))
+
+count = 0
+for index, i in enumerate(url_list):
+    count +=1
+    urlretrieve(i, "../../data/raw/download{}.pdf".format(index+1))
+print('Total {} downloaded'.format(count))
+
+pol_UNH.convert_pdf_to_txt("../../data/raw")
