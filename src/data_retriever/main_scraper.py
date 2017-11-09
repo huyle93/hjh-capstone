cfe308585ba1c514a06c7f4a9aa16f7980925eec
Huy Le <huyle.finance@gmail.com>
2017-11-09 14:02:21 -0500

    working things including collecting pdf, extract pdf and write to txt in interim

--- src/data_retriever/main_scraper.py
+++ src/data_retriever/main_scraper.py
@@ -5,15 +5,21 @@ Created on Tue Oct 31 13:37:51 2017
 
 @author: huyle
 """
+#import pol_UNH.py
+import ssl
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve
 import sys
-sys.path.insert(0, '../features')
-import pol_UNH.py
+#from PyPDF2 import PdfFileReader
+import PyPDF2
+from PyPDF2 import PdfFileReader
+#sys.path.insert(0, '../feature/pol_UNH.py')
+#from pol_UNH import converter
 
 
-from_year = input("from year: ")
-to_year = input("to year: ")
+ssl._create_default_https_context = ssl._create_unverified_context
+#from_year = input("from year: ")
+#to_year = input("to year: ")
 
 vermont_site_url = "https://www.vermontjudiciary.org/opinions-decisions?facet_from_date=01%2F01%2F"+from_year+"&facet_to_date=01%2F01%2F"+to_year+""
 
@@ -27,15 +33,41 @@ print("Data is pulling from this URL: " + vermont_site_url)
 print("="*10+"DONE"+"="*10)
 print("loading...")
 for link in pdf_url:
-#    print("="*10, count)
     url_list.append(link.find('a').attrs['href'])
-#    print(link.find('a').attrs['href'], "\n")
-#print(pdf_url.get('href'))
-
 count = 0
-for index, i in enumerate(url_list):
-    count +=1
-    urlretrieve(i, "../../data/raw/download{}.pdf".format(index+1))
+#for index, i in enumerate(url_list):
+#    count +=1
+#    urlretrieve(i, "../../data/raw/download{}.pdf".format(index+1))
 print('Total {} downloaded'.format(count))
 
-pol_UNH.convert_pdf_to_txt("../../data/raw")
+#converter.convert_pdf_to_txt("../../data/raw")
+
+mylist = []
+
+
+pdfFileObj = open('../../data/raw/download1.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+pages = pdfReader.getNumPages()
+for i in range(pages):
+    count_page = 0
+    pageObj = pdfReader.getPage(i)
+    text = str(pageObj.extractText())
+#    print(text)
+#    print("="*30+str(count)+"="*30)
+    
+    i +=1
+    count +=1
+    with open('../../data/interim/download1.txt', 'a') as f:
+        f.write(text)
+#    mylist.append(text)
+#print(mylist)
+#pageObj.extractText()
+
+#print(text)
+
+#with open('../../data/interim/download1.txt', 'w') as f:
+#    f.write(text)
+
+#inputPdf = PdfFileReader(open('../../data/raw/download1.pdf', "rb"))
+#docInfo = inputPdf.getDocumentInfo()
+#print(docInfo.title)
