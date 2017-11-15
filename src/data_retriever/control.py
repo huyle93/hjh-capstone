c5c21908f461e904541464a2ec746011bbf71a23
Huy Le <huyle.finance@gmail.com>
2017-11-15 16:17:18 -0500

    bug at text data

---
+++ src/data_retriever/control.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 17:11:25 2017
+
+@author: huyle
+"""
+
+class control:
+    
+#    def interupt:
+        
--- src/data_retriever/main_scraper.py
+++ src/data_retriever/main_scraper.py
@@ -5,69 +5,68 @@ Created on Tue Oct 31 13:37:51 2017
 
 @author: huyle
 """
-#import pol_UNH.py
+
 import ssl
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urlretrieve
-import sys
-#from PyPDF2 import PdfFileReader
 import PyPDF2
-from PyPDF2 import PdfFileReader
-#sys.path.insert(0, '../feature/pol_UNH.py')
-#from pol_UNH import converter
-
-
 ssl._create_default_https_context = ssl._create_unverified_context
-#from_year = input("from year: ")
-#to_year = input("to year: ")
 
-vermont_site_url = "https://www.vermontjudiciary.org/opinions-decisions?facet_from_date=01%2F01%2F"+from_year+"&facet_to_date=01%2F01%2F"+to_year+""
+#==============================================================================
+# 
+# #================= Data Source ====================
+# while True:
+#     try:
+#         from_year = int(input("from year: "))
+#         to_year = int(input("to year: "))
+#         if len(str(from_year)) != 4:
+#             print("Please enter year in 4 digits format")
+#         elif len(str(to_year)) != 4:
+#             print("Please enter year in 4 digits format")
+#         else:
+#             break
+#     except ValueError:
+#         print("Please enter number")
+#     
+# vermont_site_url = "https://www.vermontjudiciary.org/opinions-decisions?facet_from_date=01%2F01%2F"+str(from_year)+"&facet_to_date=01%2F01%2F"+str(to_year)+""
+# 
+# 
+# #================= Retrieve Data ====================
+# html = urlopen(vermont_site_url)
+# bsObj = BeautifulSoup(html, "html.parser")
+# pdf_url = bsObj.findAll("div", {"class": "views-field views-field-field-document"})
+# url_list = []
+# print("="*10+"STATE OF VERMONT"+"="*10)
+# print("Data is pulling from this URL: " + vermont_site_url)
+# print("="*10+"DONE"+"="*10)
+# print("retrieving data...")
+# 
+# for link in pdf_url:
+#     url_list.append(link.find('a').attrs['href'])
+# count = 0
+# for index, i in enumerate(url_list):
+#     count +=1
+#     urlretrieve(i, "../../data/raw/download{}.pdf".format(index+1))
+#     
+# print('Total {} downloaded'.format(count))
+# 
+# 
+#==============================================================================
 
-html = urlopen(vermont_site_url)
-bsObj = BeautifulSoup(html, "html.parser")
-
-pdf_url = bsObj.findAll("div", {"class": "views-field views-field-field-document"})
-url_list = []
-print("="*10+"STATE OF VERMONT"+"="*10)
-print("Data is pulling from this URL: " + vermont_site_url)
-print("="*10+"DONE"+"="*10)
-print("loading...")
-for link in pdf_url:
-    url_list.append(link.find('a').attrs['href'])
-count = 0
-#for index, i in enumerate(url_list):
-#    count +=1
-#    urlretrieve(i, "../../data/raw/download{}.pdf".format(index+1))
-print('Total {} downloaded'.format(count))
-
-#converter.convert_pdf_to_txt("../../data/raw")
-
-mylist = []
-
-
+#================= Extract PDF =====================
 pdfFileObj = open('../../data/raw/download1.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+print("extracting data to interim...")
 pages = pdfReader.getNumPages()
 for i in range(pages):
     count_page = 0
     pageObj = pdfReader.getPage(i)
     text = str(pageObj.extractText())
-#    print(text)
-#    print("="*30+str(count)+"="*30)
     
-    i +=1
-    count +=1
+#    i +=1
+#    count +=1
     with open('../../data/interim/download1.txt', 'a') as f:
         f.write(text)
-#    mylist.append(text)
-#print(mylist)
-#pageObj.extractText()
+print("txt data in interim")
 
-#print(text)
-
-#with open('../../data/interim/download1.txt', 'w') as f:
-#    f.write(text)
-
-#inputPdf = PdfFileReader(open('../../data/raw/download1.pdf', "rb"))
-#docInfo = inputPdf.getDocumentInfo()
-#print(docInfo.title)
