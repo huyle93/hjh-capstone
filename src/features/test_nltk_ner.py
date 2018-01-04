f7e28b6a419537b6c48d263f2d9551cf14994504
Huy Le <huyle.finance@gmail.com>
2018-01-03 23:31:22 -0500

    building pipeline, looped through whole file, text process finished, count words and export finished.

--- src/features/test_nltk_ner.py
+++ src/features/test_nltk_ner.py
@@ -10,39 +10,65 @@ import PyPDF2
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+# ****** pipeline *******
+# - Extract PDF to txt
+# - Process raw txt
+# -- convert to lower case
+# -- tokenize words
+# -- remove punctuation from each word
+# -- remove tokens that are not alphabetic
+# -- filter stopwords
+# - NER
+# - JSON dump
+# ***********************
+#variables
 txt_filename = 'download1.txt'
-#================= Extract PDF =====================
+count = 0
+#helping defitions
+def getTotalWords(s):
+    return len(word_tokenize(s))
+    
+#================= Extract PDF TO TXT ===============================
 try:
     pdfFileObj = open('../../data/raw/download2.pdf','rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     print("extracting data to interim...")
     pages = pdfReader.getNumPages()
-    print("="*40)
     for i in range(pages):
         pageObj = pdfReader.getPage(i)
         text = str(pageObj.extractText())
 #        print(text)
-#================= Process Text Pipeline =====================
+#================= PROCESS RAW TXT =====================
+        print("{}".format("="*30 + " TEXT PROCESS " + "="*30))
         tokens = word_tokenize(text)
+        print("tokenizing finished")
         # convert to lower case
         tokens = [w.lower() for w in tokens]
+        print("lower case finished")
         # remove punctuation from each word
         table = str.maketrans('', '', string.punctuation)
         stripped = [w.translate(table) for w in tokens]
+        print("removing punctuation finished")
         # remove remaining tokens that are not alphabetic
         words = [word for word in stripped if word.isalpha()]
+        print("removing non-alphabetic words finished")
         # filter out stop words
         stop_words = set(stopwords.words('english'))
         words = [w for w in words if not w in stop_words]
+        print("removing stopwords finished")
 #        print(words)
+        for i in words:
+            if len(word_tokenize(i)) == 1:
+                count += 1
+# export text
         for i in words:
             stripped_text = ''.join(map(str,i))+"\n"
 #            print(stripped_text)
             with open('../../data/interim/' + txt_filename, 'ab') as f:
                 f.write(stripped_text.encode('utf-8'))
-#                f.write(i.encode('utf-8'))
-
-    print("txt data in interim")
+    print("{}".format("="*30 + " EXTRACT LOG " + "="*30))
+    print("{}".format("="*12 + " total " + str(count) + " of words "))
+    print("{}".format("="*12 + " .txt file exported to interim "))
 except Exception as e:
     print("{} \n {} \n".format("="*30 + " EXTRACT LOG " + "="*30,str(e)))
     
